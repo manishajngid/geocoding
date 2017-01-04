8c260efe5e9ef416c7f8e42f3901804d4488c38d
jangid_m <jangid_m@Manish-JA.India.XoriantCorp.com>
2017-01-04 16:52:22 +0530

    Added test case for ILatLang class

---
+++ geocode/src/main/java/com/Deutsche/geocode/latlang/ILatLangTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.Deutsche.geocode.latlang;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author jangid_m
+ *
+ */
+public class ILatLangTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.Deutsche.geocode.latlang.ILatLang#getLatlangFromPostCode(int, java.lang.Boolean)}.
+	 */
+	@Test
+	public final void testGetLatlangFromPostCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
